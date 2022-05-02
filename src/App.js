2de69783ff741583d375fa868244a5fe7469dc48
numansaady <numansaady@gmail.com>
2022-05-02 16:10:58 +0600

    react router setup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './Pages/Shared/Header/Header';
+import Footer from './Pages/Shared/Footer/Footer';
+import { Route, Routes } from 'react-router-dom';
+import Home from './Pages/Home/Home/Home';
 
 function App() {
   return (
     <div className="App">
-      <h1>NS Perfume Center</h1>
+      <Header/>
+      <Routes>
+        <Route path='/' element={<Home/>}></Route>
+        <Route path='/home' element={<Home/>}></Route>
+      </Routes>
+      <Footer/>
     </div>
   );
 }
